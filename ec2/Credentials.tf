6c1891a25e54df5ebee790df8739343cb1779a7c
karthikasubramani <62528753+karthikasubramani@users.noreply.github.com>
2021-04-01 17:47:34 +0530

    Create Credentials.tf

---
+++ ec2/Credentials.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIA27Y7jjjjjjjjjj"
+}
+variable "aws_secret_key" {
+default = "A6n9qu8WrYWeUW1gggggggggggggggggggg"
+}
+variable "aws_region" {
+default = "ap-south-1"
+}
