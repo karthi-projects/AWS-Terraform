bbd97a309254debec3687065f2ec295954b919d7
karthikasubramani <62528753+karthikasubramani@users.noreply.github.com>
2021-04-01 17:49:52 +0530

    Create var.tf

---
+++ ec2/var.tf
@@ -0,0 +1,27 @@
+variable "subnet" {
+default = "subnet-141d7658"
+}
+variable "storage" {
+default = "10"
+}
+variable "oidsecgroup" {
+default = "sg-0cebabc4005cc8d12"
+}
+variable "ami" {
+default = "ami-0a9d27a9f4f5c0efc"
+}
+variable "nametag" {
+default = "testserver"
+}
+variable "keypair" {
+default = "sathvik"
+}
+variable "instancetype" {
+default = "t2.micro"
+}
+variable "region" {
+default = "ap-south"
+}
+variable "srvrcount" {
+default = "2"
+}
