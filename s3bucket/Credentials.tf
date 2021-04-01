11e27766a1ad42bd794f22fd4bdb16a2e872ca49
karthikasubramani <62528753+karthikasubramani@users.noreply.github.com>
2021-04-01 17:45:00 +0530

    Update Credentials.tf

--- s3bucket/Credentials.tf
+++ s3bucket/Credentials.tf
@@ -1,1 +1,10 @@
+variable "aws_access_key" {
+default = "jhhkhklkhkhkhkhkhhh"
+}
+variable "aws_secret_key" {
+default = "A6n9quhhhhhhhhhW1OFlriiiiiiiii1"
+}
+variable "aws_region" {
+default = "ap-south-1"
+}
 
